b6e1de1682d88cee33f62d632e6bd5f1ca3a1080
themodernmonk7 <sauravavishek@gmail.com>
2023-06-18 12:29:49 +0530

    feat: drag and drop

--- src/components/TaskBoard.jsx
+++ src/components/TaskBoard.jsx
@@ -7,42 +7,57 @@ import data from '../utils/data';
 import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
 
 const TaskBoard = () => {
+  const [boardData, setBoardData] = useState(data);
 
   const handleDragEnd = (result) => {
-    // console.log(result);
-    const {source, destination} = result 
-    if(!destination) return 
-    if(destination.droppableId === source.droppableId && destination.index === source.index) return
-    
+    const { source, destination } = result;
 
-  }
-  
+    if (!destination) return;
+    if (destination.droppableId === source.droppableId && destination.index === source.index) return;
+
+    // Find the source section and task
+    const sourceSectionIndex = source.droppableId;
+    const sourceTaskIndex = source.index;
+    const sourceSection = boardData[sourceSectionIndex];
+    const sourceTask = sourceSection.tasks[sourceTaskIndex];
+
+    // Remove the task from the source section
+    sourceSection.tasks.splice(sourceTaskIndex, 1);
+
+    // Find the destination section
+    const destinationSectionIndex = destination.droppableId;
+    const destinationSection = boardData[destinationSectionIndex];
+
+    // Add the task to the destination section
+    destinationSection.tasks.splice(destination.index, 0, sourceTask);
+
+    // Update the state with the modified data
+    setBoardData([...boardData]);
+  };
+
   return (
     <DragDropContext onDragEnd={handleDragEnd}>
+      <section className="grid grid-cols-3 gap-5">
+        {boardData.map((item, sectionIndex) => {
+          const { id, title: text, identityColor: markColor, tasks } = item;
+          return (
+            <div key={id} className="rounded-2xl bg-[#F5F5F5] p-5">
+              <div className="flex items-center space-x-2">
+                <span
+                  style={{ backgroundColor: `${markColor}` }}
+                  className={`h-2 w-2 rounded-full`}
+                ></span>
+                <p className="font-medium capitalize text-blackColor">{text}</p>
+                <span className="flex h-5 w-5 items-center justify-center rounded-full bg-[#E0E0E0] text-xs font-medium">
+                  {tasks.length}
+                </span>
+              </div>
+              <hr
+                style={{ borderColor: `${markColor}` }}
+                className={`mt-5 border-2 border-[#${markColor}]`}
+              />
 
-    <section className="grid grid-cols-3 gap-5">
-      {data.map((item) => {
-        const { id, title: text, identityColor: markColor, tasks } = item;
-        return (
-          <div key={id} className="rounded-2xl bg-[#F5F5F5] p-5">
-            <div className="flex items-center space-x-2">
-              <span
-                style={{ backgroundColor: `${markColor}` }}
-                className={`h-2 w-2 rounded-full`}
-              ></span>
-              <p className="font-medium capitalize text-blackColor">{text}</p>
-              <span className="flex h-5 w-5 items-center justify-center rounded-full bg-[#E0E0E0] text-xs font-medium">
-                {tasks.length}
-              </span>
-            </div>
-            <hr
-              style={{ borderColor: `${markColor}` }}
-              className={`mt-5 border-2 border-[#${markColor}]`}
-            />
-
-            {/* Card */}
-              {/* <Droppable droppableId="card"> */}
-              <Droppable droppableId={text}>
+              <Droppable droppableId={sectionIndex.toString()}>
                 {(provided) => (
                   <section {...provided.droppableProps} ref={provided.innerRef}>
                     {tasks.map((task, index) => {
@@ -57,76 +72,76 @@ const TaskBoard = () => {
                         users,
                       } = task;
                       return (
-                        <Draggable key={id} draggableId={id.toString()} index={index}  >
+                        <Draggable key={id} draggableId={id.toString()} index={index}>
                           {(provided) => (
                             <article
-                              {...provided.draggableProps}
-                              {...provided.dragHandleProps}
-                              ref={provided.innerRef}
-                              className={`my-5 rounded-2xl bg-white p-8  `}
-                            >
-                              <div className="flex items-center justify-between ">
-                                <span
-                                  className={`rounded px-2 py-1 text-xs font-medium capitalize ${
-                                    priority === 'low' && 'bg-[#DFA87433] text-[#D58D49]'
-                                  } ${priority === 'high' && 'bg-[#D8727D1A] text-[#D8727D] '}  ${priority === 'completed' && 'bg-[#83C29D33] text-[#68B266]'}`}
-                                >
-                                  {priority}
-                                </span>
-                                <button className="text-2xl">...</button>
-                              </div>
-                              <h3 className="mt-2 text-lg font-semibold text-blackColor">{title}</h3>
-                              {image && (
-                                <div className="mt-5">
-                                  <img
-                                    src={image}
-                                    alt=""
-                                    className={`h-40 w-full object-cover ${
-                                      priority === 'completed' && 'h-64 object-top'
-                                    }`}
-                                  />
-                                </div>
-                              )}
-                              {description && (
-                                <p
-                                  className={`mt-2 ${
-                                    priority === 'completed' ? 'text-[#0D062D' : 'text-grayColor'
+                            {...provided.draggableProps}
+                            {...provided.dragHandleProps}
+                            ref={provided.innerRef}
+                            className={`my-5 rounded-2xl bg-white p-8   `}
+                          >
+                            <div className="flex items-center justify-between ">
+                              <span
+                                className={`rounded px-2 py-1 text-xs font-medium capitalize ${
+                                  priority === 'low' && 'bg-[#DFA87433] text-[#D58D49]'
+                                } ${priority === 'high' && 'bg-[#D8727D1A] text-[#D8727D] '}  ${priority === 'completed' && 'bg-[#83C29D33] text-[#68B266]'}`}
+                              >
+                                {priority}
+                              </span>
+                              <button className="text-2xl">...</button>
+                            </div>
+                            <h3 className="mt-2 text-lg font-semibold text-blackColor">{title}</h3>
+                            {image && (
+                              <div className="mt-5">
+                                <img
+                                  src={image}
+                                  alt=""
+                                  className={`h-40 w-full object-cover ${
+                                    priority === 'completed' && 'h-64 object-top'
                                   }`}
-                                >
-                                  {description}
-                                </p>
-                              )}
+                                />
+                              </div>
+                            )}
+                            {description && (
+                              <p
+                                className={`mt-2 ${
+                                  priority === 'completed' ? 'text-[#0D062D' : 'text-grayColor'
+                                }`}
+                              >
+                                {description}
+                              </p>
+                            )}
 
-                              <div className="mt-10 flex items-center justify-between">
-                                <div className="flex -space-x-2 overflow-hidden">
-                                  <img
-                                    className="inline-block h-6 w-6 rounded-full ring-2 ring-white"
-                                    src={user5}
-                                    alt="{user.handle}"
-                                  />
-                                  <img
-                                    className="inline-block h-6 w-6 rounded-full ring-2 ring-white"
-                                    src={user4}
-                                    alt="{user.handle}"
-                                  />
-                                  <img
-                                    className="inline-block h-6 w-6 rounded-full ring-2 ring-white"
-                                    src={user3}
-                                    alt="{user.handle}"
-                                  />
+                            <div className="mt-10 flex items-center justify-between">
+                              <div className="flex -space-x-2 overflow-hidden">
+                                <img
+                                  className="inline-block h-6 w-6 rounded-full ring-2 ring-white"
+                                  src={user5}
+                                  alt="{user.handle}"
+                                />
+                                <img
+                                  className="inline-block h-6 w-6 rounded-full ring-2 ring-white"
+                                  src={user4}
+                                  alt="{user.handle}"
+                                />
+                                <img
+                                  className="inline-block h-6 w-6 rounded-full ring-2 ring-white"
+                                  src={user3}
+                                  alt="{user.handle}"
+                                />
+                              </div>
+                              <div className="flex items-center justify-center space-x-5 text-xs font-medium text-grayColor">
+                                <div className="flex items-center justify-center space-x-2">
+                                  <Message />
+                                  <span className="">{comments} comments</span>
                                 </div>
-                                <div className="flex items-center justify-center space-x-5 text-xs font-medium text-grayColor">
-                                  <div className="flex items-center justify-center space-x-2">
-                                    <Message />
-                                    <span className="">{comments} comments</span>
-                                  </div>
-                                  <div className="flex items-center justify-center space-x-2">
-                                    <FolderMinus />
-                                    <span className="">{files} files</span>
-                                  </div>
+                                <div className="flex items-center justify-center space-x-2">
+                                  <FolderMinus />
+                                  <span className="">{files} files</span>
                                 </div>
                               </div>
-                            </article>
+                            </div>
+                          </article>
                           )}
                         </Draggable>
                       );
@@ -135,11 +150,11 @@ const TaskBoard = () => {
                   </section>
                 )}
               </Droppable>
-          </div>
-        );
-      })}
-    </section>
-      </DragDropContext>
+            </div>
+          );
+        })}
+      </section>
+    </DragDropContext>
   );
 };
 
